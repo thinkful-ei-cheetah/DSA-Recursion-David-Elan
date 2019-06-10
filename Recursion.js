67a0df6f6171419f11c470950d83b3abd2f8b265
David Nordeen <davidnordeen@gmail.com>
2019-06-10 09:40:23 -0700

    1&2

--- Recursion.js
+++ Recursion.js
@@ -1,1 +1,30 @@
-hello
+'use strict';
+
+//1. Counting Sheep. Write a function that counts how many sheep jump over the fence
+
+const countSheep = function (sheep) {
+  if (sheep < 0) {
+    return;
+  }
+  else if (sheep > 0) {
+    console.log(`${sheep}: another sheep jump over the fence`);
+    countSheep(sheep - 1);
+  }
+  else {
+    console.log('All sheep jumped over the fence');
+  }
+};
+
+//2. Power Calculator
+
+const powerCalculator = function(base, exp){
+  if(exp === 0){
+    return base;
+  }
+  if (exp < 0){
+    return 'exponent should be >= 0';
+  }
+  else {
+    return base * powerCalculator(base, exp-1);
+  }
+};
